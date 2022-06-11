8ab4bf58b4da33e0f26fc9d1e0059b5fcd4a6fa2
huseyinafsin <huseyinafsin4@gmail.com>
2022-06-11 10:07:49 +0300

    Login sayfası uyarı mesajı

--- lib/login.dart
+++ lib/login.dart
@@ -3,104 +3,115 @@ import 'package:chat_app/register.dart';
 import 'package:chat_app/server/auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-  final TextEditingController emailController=TextEditingController();
-  final TextEditingController passController=TextEditingController();
-  AuthServer authServer=AuthServer();
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+  String errorText = "";
+  late BuildContext _context;
+  AuthServer authServer = AuthServer();
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    _context = context;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        width:double.infinity,
+        width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-                Colors.cyan,
-                Colors.cyan,
-                Colors.cyan
-          ]),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [Colors.cyan, Colors.cyan, Colors.cyan]),
         ),
         child: ListView(
           children: <Widget>[
-            SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+            ),
             Padding(
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Center(
-                    child: Text("Giriş yap",style: TextStyle(color: Colors.white,fontSize: 40),),
+                    child: Text(
+                      "Giriş yap",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Center(
-                    child: Text("Hoş geldiniz",style: TextStyle(color: Colors.white,fontSize: 18),),
+                    child: Text(
+                      "Hoş geldiniz",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
                   )
                 ],
               ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height*0.8,
+              height: MediaQuery.of(context).size.height * 0.8,
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(60),
                     topRight: Radius.circular(60),
-                  )
-              ),
+                  )),
               child: Padding(
                 padding: EdgeInsets.all(30),
                 child: Column(
-                  children:<Widget>[
-                    SizedBox(height: 40,),
+                  children: <Widget>[
+                    SizedBox(
+                      height: 40,
+                    ),
                     Container(
                       decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)
-                      ),
+                          borderRadius: BorderRadius.circular(10)),
                       child: Column(
-                        children:<Widget>[
+                        children: <Widget>[
                           Container(
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
                                 border: Border(
-                                    bottom: BorderSide(color: Colors.grey)
-                                )
-                            ),
+                                    bottom: BorderSide(color: Colors.grey))),
                             child: TextField(
                               controller: emailController,
                               decoration: InputDecoration(
+                                  errorText: errorText,
                                   hintText: "E-postanız giriniz",
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none
-                              ),
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  border: InputBorder.none),
                             ),
                           ),
                           Container(
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
                                 border: Border(
-                                    bottom: BorderSide(color: Colors.grey)
-                                )
-                            ),
+                                    bottom: BorderSide(color: Colors.grey))),
                             child: TextField(
                               controller: passController,
                               decoration: InputDecoration(
                                   hintText: "Şifre giriniz",
                                   hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none
-                              ),
+                                  border: InputBorder.none),
                             ),
                           ),
-
                         ],
                       ),
                     ),
-                    SizedBox(height: 40,),
+                    SizedBox(
+                      height: 40,
+                    ),
                     Text(
                       "Şifreniz Unuttunuz mu?",
                       style: TextStyle(color: Colors.grey),
                     ),
-                    SizedBox(height: 40,),
+                    SizedBox(
+                      height: 40,
+                    ),
                     InkWell(
                       child: Container(
                           height: 50,
@@ -110,43 +121,68 @@ class LoginPage extends StatelessWidget{
                             borderRadius: BorderRadius.circular(10),
                           ),
                           child: Center(
-                            child: Text("Giriş",style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold
-                            ),),
+                            child: Text(
+                              "Giriş",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           )),
-                      onTap: ()async{
+                      onTap: () async {
                         print(emailController.text);
-                        var user = await authServer
-                            .SignIn(emailController.text,
-                            passController.text);
-                        if(user!=null){
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Users()));
+                        var user = await authServer.SignIn(
+                            emailController.text, passController.text);
+                        if (user != null) {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => Users()));
+                        } else {
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('Uyarı!'),
+                              content: const Text(
+                                  'Kullanıcı Bulunamadı, e-posta veya şifrenizi kontrol ediniz'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Tamam'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          );
                         }
                       },
                     ),
-                    SizedBox(height: 40,),
+                    SizedBox(
+                      height: 40,
+                    ),
                     Container(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Hesapınız yoksa ? ",style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15
-                          ),),
-                          SizedBox(width: 5,),
+                          Text(
+                            "Hesapınız yoksa ? ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
                           InkWell(
                             child: Text(
                               "tıklayın ",
                               style: TextStyle(
                                   color: Colors.cyan,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 15
-                              ),
+                                  fontSize: 15),
                             ),
-                            onTap: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=>Register()));
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Register()));
                             },
                           ),
                         ],
